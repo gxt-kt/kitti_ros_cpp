d337c632a91544fca0c4085ce22f15b0f5b2606e
gxt_kt <gxt_kt@163.com>
2023-03-30 15:28:29 +0000

    add lidar cloud_point

---
+++ src/kitti_ros/include/kitti_ros/read_cloudpoint_bin.hpp
@@ -0,0 +1,63 @@
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "kitti_ros/debugstream.hpp"
+
+struct Point {
+  float x;
+  float y;
+  float z;
+  float intensity;  //
+};
+
+inline bool ConvertKittiBin2Point(std::string filename,
+                                  std::vector<Point>& cloud_points) {
+  std::ifstream input(filename, std::ios::binary);
+  if (!input.good()) {
+    std::cerr << "Could not read file: " << filename << std::endl;
+    return 1;
+  }
+
+  Point point;
+
+  while (input.read((char*)&point, sizeof(Point))) {
+    cloud_points.push_back(point);
+  }
+
+  input.close();
+
+  std::cout << "size=" << cloud_points.size();
+
+  // 在这里对点云数据进行处理，例如使用PCL库将其转换为pcl::PointCloud<pcl::PointXYZI>类型
+
+  return 0;
+}
+
+
+inline bool ConvertKittiBin2PclPointCloud(std::string filename, pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud) {
+  std::ifstream input(filename, std::ios::binary);
+  if (!input.good()) {
+    std::cerr << "Could not read file: " << filename << std::endl;
+    return 1;
+  }
+
+  Point point;
+  while (input.read((char*)&point, sizeof(Point))) {
+    pcl::PointXYZI point_pcl;
+    point_pcl.x = point.x;
+    point_pcl.y = point.y;
+    point_pcl.z = point.z;
+    point_pcl.intensity = point_pcl.intensity;
+    cloud->push_back(point_pcl);
+  }
+
+  input.close();
+
+  return 0;
+}
--- src/kitti_ros/src/kitti_main.cc
+++ src/kitti_ros/src/kitti_main.cc
@@ -1,12 +1,14 @@
-#include "ros/ros.h"
 #include <string>
 #include <vector>
 
+#include "ros/ros.h"
+
 // use cv_bridge to publish image
+#include <opencv2/highgui.hpp>
+
 #include "cv_bridge/cv_bridge.h"
 #include "image_transport/image_transport.h"
 #include "opencv2/core.hpp"
-#include <opencv2/highgui.hpp>
 #include "opencv2/opencv.hpp"
 #include "opencv2/videoio.hpp"
 
@@ -15,49 +17,47 @@
 
 // debugstream by gxt_kt
 #include "kitti_ros/debugstream.hpp"
+#include "kitti_ros/read_cloudpoint_bin.hpp"
 
 // dataset directory
 std::string dir =
-    "/media/home/2011_09_26_drive_0005_sync/2011_09_26/";
-//
-std::string image_dir=dir+ "2011_09_26_drive_0005_sync/image_02/data/";
+    "/media/home/2011_09_26_drive_0005_sync/2011_09_26/"
+    "2011_09_26_drive_0005_sync/";
+
+std::string image_dir = dir + "image_02/data/";
+std::string lidar_dir = dir + "velodyne_points/data/";
 
 int main(int argc, char *argv[]) {
   ros::init(argc, argv, "main_publish");
   ros::NodeHandle nh;
-  gDebug << "hello world";
-
-  std::vector<cv::String> fn;
-  glob(image_dir + "*.png", fn, false);
-
-  // cv::Mat im=cv::imread(fn[0]);
-  // if (im.empty()) {
-  //   gDebug << FATAL_ERROR << "Could not read the image: " << image_path;
-  //   return 1;
-  // }
-  // imshow("image", im);
-  // while ((cv::waitKey(0) & 0xFF) != 'q') {}
+  gDebug << "main_publish";
 
   // image pub
   image_transport::ImageTransport it(nh);
   image_transport::Publisher image_pub = it.advertise("camera02/image", 10);
 
-
   // cloudpoint pub
-  ros::Publisher cloud_pub = nh.advertise<sensor_msgs::PointCloud>("cloudpoint", 50);
-
+  ros::Publisher cloud_pub =
+      nh.advertise<sensor_msgs::PointCloud2>("cloudpoint", 50);
 
 
   ros::Rate loop_rate(10);
   while (nh.ok()) {
-    {// send_image
-      static size_t image_i{0};
-      if (image_i >= fn.size()) image_i = 0;
-      cv::Mat image = cv::imread(fn[image_i]);
+
+    // get current dataset sequence i
+    static int kitti_i{0};
+    kitti_i %= 154;
+    std::stringstream ss;
+    ss << std::setw(10) << std::setfill('0') << std::to_string(kitti_i);
+    std::string katti_num = ss.str();
+    kitti_i++;
+
+    {  // send_image
+      cv::Mat image = cv::imread(image_dir + katti_num + ".png");
       // cv::imshow("image",image);
       if (image.empty()) {
         gDebug << FATAL_ERROR << "error image is empty";
-        return 1;  // only proceed if sucsessful
+        return 1;
       }
       std_msgs::Header header;
       header.stamp = ros::Time::now();
@@ -65,12 +65,21 @@ int main(int argc, char *argv[]) {
       sensor_msgs::ImagePtr msg =
           cv_bridge::CvImage(header, "bgr8", image).toImageMsg();
       image_pub.publish(msg);
-      gDebug << "send message" << image_i;
-      ++image_i;
     }
 
+    {  // send lidar cloud_point
+      pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(
+          new pcl::PointCloud<pcl::PointXYZI>);
+      ConvertKittiBin2PclPointCloud(lidar_dir + katti_num+".bin", cloud);
+      sensor_msgs::PointCloud2 ros_cloud;
+      pcl::toROSMsg(*cloud, ros_cloud);
+      ros_cloud.header.frame_id = "map";
+      ros_cloud.header.stamp = ros::Time::now();
+      cloud_pub.publish(ros_cloud);
+    }
+
+    gDebug << "send kitti dataset i =" << kitti_i;
     ros::spinOnce();
     loop_rate.sleep();
   }
-
 }
